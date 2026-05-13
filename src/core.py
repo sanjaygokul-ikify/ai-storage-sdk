16b1b00da1c522a0fcfda8d4f12cb47026f23a50
Sanjay G <gokulesh2615@gmail.com>
2026-05-13 20:05:25 +0530

    feat: add src/core.py

---
+++ src/core.py
@@ -0,0 +1,12 @@
+import os
+import logging
+
+class StorageSDK:
+    def __init__(self):
+        self.storage = {}
+
+    def store(self, key, value):
+        self.storage[key] = value
+
+    def retrieve(self, key):
+        return self.storage.get(key)
